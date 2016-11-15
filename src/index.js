fd111b984fddfdbafc0d2681d0263ff0bcd5e41b
Richard van der Dys <richard.vanderdys@gmail.com>
2016-11-15 17:18:53 -0600

    Got webpack working

---
+++ src/index.js
@@ -0,0 +1,13 @@
+console.log('hello');
+
+const pandaImageUrl = 'http://assets.worldwildlife.org/photos/81/images/carousel_small/Giant_Panda_Why_They_Matter_image_(c)_Bernard_De_Wetter_WWF_Canon.jpg?1345563751';
+
+const shader = `
+void main() {
+  gl_FragColor = vec4(1.0,  // R
+                      0.0,  // G
+                      1.0,  // B
+                      1.0); // A
+}`;
+
+document.body.append('Hi');
