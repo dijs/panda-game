ba3d4946730b99613d027286bf3bdf2da46e335a
Richard van der Dys <richard.vanderdys@gmail.com>
2016-11-20 08:27:33 -0600

    Got a box rendering

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-console.log('hello');
+import {
+  Scene, PerspectiveCamera, CubeGeometry, MeshNormalMaterial, Mesh,
+  WebGLRenderer, MeshBasicMaterial, BoxGeometry
+} from 'three';
 
 const pandaImageUrl = 'http://assets.worldwildlife.org/photos/81/images/carousel_small/Giant_Panda_Why_They_Matter_image_(c)_Bernard_De_Wetter_WWF_Canon.jpg?1345563751';
 
@@ -8,6 +11,40 @@ void main() {
                       0.0,  // G
                       1.0,  // B
                       1.0); // A
-}`;
+}
+`;
 
-document.body.append('Hi');
+const scene = new Scene();
+
+const camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.z = 5;
+
+scene.add(camera);
+
+const geometry = new BoxGeometry(1, 1, 1);
+// const material = new MeshBasicMaterial({
+//   color: 0xff0000,
+//   wireframe: true
+// });
+const material = new MeshNormalMaterial();
+
+const mesh = new Mesh(geometry, material);
+scene.add(mesh);
+
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  render();
+}
+
+function render() {
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
+  renderer.render(scene, camera);
+}
+
+animate();
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         include: path.join(__dirname, 'src'),
         loader: 'babel-loader',
         query: {
-          presets: ["es2015"],
+          presets: ['es2015'],
         }
       }
     ]
